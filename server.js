23974f18c84d882abd7a987d366530c6865a27a6
krisrimar <rimarkris@gmail.com>
2016-01-02 16:41:54 +0200

    Updated server.js (connect node.js framework)
    - added 'connect' framework;
    - added doFirst and doSecond functions that demonstrate how the code
    will run one after another;
    - commented out the doFirst and do Second functions to try out the
    app.use('/profile', profile) and profile functions;
    
    Main functions used:
    
    var app = connect();
    
    next();
    
    app.use();
    
    http.createServer(app).listen(80);

--- server.js
+++ server.js
@@ -1,35 +1,35 @@
 var http = require("http");
 var fs = require("fs");
+var connect = require("connect");
 
-//Send a 404 response if things go bad
-function send404Response(response) {
-    response.writeHead(404, {"Conten-Type": "text/plain"});
-    response.write("Error 404: Page not found");
-    response.end();
+var app = connect();
+
+//Run one block of code after another:
+
+/*
+
+function doFirst(request, response, next) {
+    console.log("Bacon");
+    next(); // if it has this method it will go to the next object in the stack
 }
 
-//Handle a user request
-function onRequest(request, response) {
-
-    // if the user's request is a GET request and if the user is going to the homepage
-
-    if(request.method == 'GET' && request.url == '/') { 
-
-        // send a success status and content type of html (because we need more than plain text now)
-
-        response.writeHead(200, {"Conten-Type": "text/html"}); 
-
-        // read the file "index.html" and stream it / write it / response it through the response object
-
-        fs.createReadStream("./index.html").pipe(response); 
-        
-        //if the user tries to connect to another page that doesn't exist
-        
-    } else {
-        send404Response(response);
-    }
+function doSecond(request, response, next) {
+    console.log("Tuna");
+    next();
 }
 
-//Create a server and listen for requests on this port
-http.createServer(onRequest).listen(80);
-console.log("Server is now running...");
+app.use(doFirst);
+app.use(doSecond);
+
+*/
+
+//Run certain code on user's request:
+
+function profile(request, response) {
+    console.log("The user requested profile");
+}
+
+app.use('/profile', profile);
+
+http.createServer(app).listen(80);
+console.log("Server is running...");
