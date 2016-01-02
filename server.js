77f7257ce9b5bcf528d6db933193d70839025236
krisrimar <rimarkris@gmail.com>
2016-01-02 13:40:57 +0200

    Modified the server file
    - added more explanation to what is happening
    - added a console log of a user's request
    - changed the port from 7000 to 80 (the default port)

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
-var http = require('http');
+var http = require("http");
 
-var server = http.createServer(function (request, response) {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("Hello World\n");
-});
+//This is the callback method, is called every time a user makes a request
+//Request object has info about their request, response object is what we send back to them
+function onRequest(request, response) {
+    console.log("A user made a request " + request.url);
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("I am a simple Node server!");
+    response.end();
+}
 
-server.listen(7000);
+//Create a server and listen for requests on this port
+http.createServer(onRequest).listen(80);
+console.log("Server is now running...");
+
+//Now open Chrome and go to http://localhost:8888
+//Saying that user made request twice because browser also makes a request for the favicon
