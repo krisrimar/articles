6bcbe5dd7be504fa1e0ab60c78951162a4c34d5c
krisrimar <rimarkris@gmail.com>
2016-01-02 14:14:18 +0200

    Added index.html / updated server.js
    - added index.html for testing the new file system on the server;
    - added the fs module to the server;
    - added index.html output to the server;
    - added 404 response to the server if the HTTP method is not GET or the
    user is not visiting the home page (index.html);
    
    Main functions used:
    
    fs.createReadStream("./index.html").pipe(response);

--- server.js
+++ server.js
@@ -1,17 +1,35 @@
 var http = require("http");
+var fs = require("fs");
 
-//This is the callback method, is called every time a user makes a request
-//Request object has info about their request, response object is what we send back to them
-function onRequest(request, response) {
-    console.log("A user made a request " + request.url);
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("I am a simple Node server!");
+//Send a 404 response if things go bad
+function send404Response(response) {
+    response.writeHead(404, {"Conten-Type": "text/plain"});
+    response.write("Error 404: Page not found");
     response.end();
+}
+
+//Handle a user request
+function onRequest(request, response) {
+
+    // if the user's request is a GET request and if the user is going to the homepage
+
+    if(request.method == 'GET' && request.url == '/') { 
+
+        // send a success status and content type of html (because we need more than plain text now)
+
+        response.writeHead(200, {"Conten-Type": "text/html"}); 
+
+        // read the file "index.html" and stream it / write it / response it through the response object
+
+        fs.createReadStream("./index.html").pipe(response); 
+        
+        //if the user tries to connect to another page that doesn't exist
+        
+    } else {
+        send404Response(response);
+    }
 }
 
 //Create a server and listen for requests on this port
 http.createServer(onRequest).listen(80);
 console.log("Server is now running...");
-
-//Now open Chrome and go to http://localhost:8888
-//Saying that user made request twice because browser also makes a request for the favicon
